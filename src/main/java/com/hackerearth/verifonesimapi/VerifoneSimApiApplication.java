32c0770dbff02ea9869a15c9ad92874217dd0720
Somaditya Basak <somaditya.b@gmail.com>
2022-08-21 22:41:32 +0530

    init

---
+++ src/main/java/com/hackerearth/verifonesimapi/VerifoneSimApiApplication.java
@@ -0,0 +1,13 @@
+package com.hackerearth.verifonesimapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerifoneSimApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VerifoneSimApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hackerearth/verifonesimapi/VerifoneSimApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackerearth.verifonesimapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VerifoneSimApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
